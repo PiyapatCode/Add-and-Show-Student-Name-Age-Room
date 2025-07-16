d31747d9c436368501711990ddf5194e0236d2a2
PiyapatCode <endthewin2549@gmail.com>
2025-07-16 17:21:01 +0700

    Update Class2.cs

--- Class2.cs
+++ Class2.cs
@@ -1,11 +1,11 @@
-﻿using System;
+using System;
 namespace Studentpro {
     class StorageStudentInfo {
    public string Name { get; set; }
-        public int Age { get; set; }
+        public string Age { get; set; }
         public string Room { get; set; }
         public StorageStudentInfo() { }
-        public StorageStudentInfo(string name, int age, string room) {
+        public StorageStudentInfo(string name, string age, string room) {
             Name = name;
             Age = age;
             Room = room;
